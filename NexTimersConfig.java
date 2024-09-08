28dc0c4fed6ba6a26e25b12424f05b2e644da93c
imKwok <kwok.deric@gmail.com>
2024-09-08 16:08:41 -0400

    Update NexTimersConfig.java

--- NexTimersConfig.java
+++ NexTimersConfig.java
@@ -5,45 +5,40 @@ import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("nextimers")
-public interface NexTimersConfig extends Config
-{
+public interface NexTimersConfig extends Config {
 	@ConfigItem(
 		keyName = "combineTime",
 		name = "Combine Boss & Minion Times",
 		description = "Combines the splits of Nex and her minions."
 	)
-	default boolean combineTime()
-	{
+	default boolean combineTime() {
 		return false;
 	}
 
 	@ConfigItem(
-			keyName = "displayOverlay",
-			name = "Display Overlay",
-			description = "Displays splits overlay."
+		keyName = "displayOverlay",
+		name = "Display Overlay",
+		description = "Displays splits overlay."
 	)
-	default boolean displayOverlay()
-	{
+	default boolean displayOverlay() {
 		return true;
 	}
 
 	@ConfigItem(
-			keyName = "displayTimePrecise",
-			name = "Precise Time",
-			description = "Sets timer to be precise."
+		keyName = "displayTimePrecise",
+		name = "Precise Time",
+		description = "Sets timer to be precise."
 	)
-	default boolean displayTimePrecise()
-	{
+	default boolean displayTimePrecise() {
 		return true;
 	}
 
 	@ConfigItem(
-			keyName = "sendChatMessage",
-			name = "Game Chat Report",
-			description = "Sends a message of all splits in game chat."
+		keyName = "sendChatMessage",
+		name = "Game Chat Report",
+		description = "Sends a message of all splits in game chat."
 	)
-	default boolean sendChatMessage()
-	{
+	default boolean sendChatMessage() {
 		return true;
 	}
 }
